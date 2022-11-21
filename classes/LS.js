2f4b9f1b9f210e929081f3cf8db9815e33193f3a
Kermo <kermo.ohno@voco.com>
2022-11-21 20:07:53 +0200

    oop_books valmis

---
+++ classes/LS.js
@@ -0,0 +1,31 @@
+class LS{
+    getData(key){
+        let data
+        if(localStorage.getItem(key) === null){
+            data = []
+        } else {
+            data = JSON.parse(localStorage.getItem(key))
+        }
+        return data
+    }
+
+    setData(key, data){
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+
+    addBook(book){
+        const books = this.getData('books')
+        books.push(book)
+        this.setData('books', books)
+    }
+
+    delBook(book){
+        let books = this.getData('books')
+        books.forEach(function(bookFromLS, bookIndex){
+            if(bookFromLS.title === book.title && bookFromLS.author === book.author && bookFromLS.isbn === book.isbn){
+                books.splice(bookIndex, 1)
+            }
+        })
+        this.setData('books', books)
+    }
+}
---
+++ classes/UI.js
@@ -0,0 +1,45 @@
+class UI {
+    constructor() {
+        this.title = document.querySelector('#title')
+        this.author = document.querySelector('#author')
+        this.isbn = document.querySelector('#isbn')
+        this.bookList = document.querySelector('#book-list')
+    }
+    addBook(book){
+        const tr = document.createElement('tr')
+        tr.setAttribute('class', 'book-row')
+        tr.innerHTML = `<td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.isbn}</td>
+                <td><a href="#">X</a></td>`
+        this.bookList.appendChild(tr)
+    }
+
+    getBook(click){
+        let isbn = click.parentElement.previousElementSibling.textContent
+        let author = click.parentElement.previousElementSibling.previousElementSibling.textContent
+        let title = click.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+        const book = new Book(title, author, isbn)
+        return book
+    }
+
+    delBook(click){
+        if(confirm('Do you realy want to delete this book?')) {
+            click.parentElement.parentElement.remove()
+            return true
+        } else {
+            return false
+        }
+    }
+
+    filterData(filter){
+        let bookListData = document.querySelectorAll('.book-row')
+        bookListData.forEach(function (trContent){
+            if(trContent.textContent.toLowerCase().indexOf(filter) !== -1){
+                trContent.style.display = '';
+            } else {
+                trContent.style.display = 'none'
+            }
+        })
+    }
+}
