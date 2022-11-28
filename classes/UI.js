298436d4b9764c9e286884f8d02d6f597ac6d173
Kermo <kermo.ohno@voco.com>
2022-11-28 22:09:28 +0200

    Refactored addbook functionality

--- classes/UI.js
+++ classes/UI.js
@@ -1,29 +1,38 @@
 class UI {
+    // help function to add DOM element
+    addUIElement(name, classname = '', textcontent = '', atributes = {}){
+        // create element
+        const element = document.createElement(name);
+        // add css style class to element
+        if(classname !== ''){
+            element.className = classname
+        }
+        // add text content to element
+        element.appendChild(document.createTextNode(textcontent))
+        // add atributes to element
+        if(Object.keys(atributes).length > 0){
+            for(let key in atributes){
+                element.setAttribute(key, atributes[key])
+            }
+        }
+        return element
+    }
     addBook(book){
         // create <tr> element
-        const tr = document.createElement('tr');
+        const tr = this.addUIElement('tr');
         // td for title, author and isbn
         for(let name in book){
-            // create <td> element
-            const td = document.createElement('td');
-            // create text element
-            let text = document.createTextNode(book[name]);
-            // add text to <td>
-            td.appendChild(text);
+            // create <td> element and add text
+            let td = this.addUIElement('td', '', book[name]);
             // add td to tr
             tr.appendChild(td);
-            console.log(tr)
         }
         // X link
         // create <td> element
-        let td = document.createElement('td');
+        let td = this.addUIElement('td');
         // create <a> element
-        const link = document.createElement('a');
-        // set href atribute to <a>
-        link.setAttribute('href','#');
-        // add text content to <a>
-        link.appendChild(document.createTextNode('X'));
-        // add <a> to <li>
+        const link = this.addUIElement('a', '', 'X', {'href':'#'});
+        // add link to td
         td.appendChild(link);
         // add td to tr
         tr.appendChild(td);
